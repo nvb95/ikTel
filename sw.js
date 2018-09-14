5635982b413303d575576e018959f21de4023221
Niels Van Belle <niels.vanbelle@hotmail.com>
2018-09-14 11:11:44 +0200

    check service worker

--- sw.js
+++ sw.js
@@ -72,6 +72,7 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
+      alert("all cashed");
       return cache.addAll(filesToCache);
     })
   );
